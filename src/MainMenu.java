ee581c9bcafbfa992023bebaeb1c1fbee331ded7
Patrick Nyboe Kjøller <Patrick@ctrl.dk>
2023-12-13 17:56:05 +0100

    Update2

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -67,7 +67,7 @@ public class MainMenu {
                 break;
             default:
                 ui.displayMessage("Invalid choice. Please try again.");
-                menu();
+                settingsMenu();
                 break;
         }
     }
