f29a7e3a9a9b09bfb7f79954fdeb90d7dda59c9d
cph-sm522 <cph-sm522@cphbusiness.dk>
2023-12-13 11:19:14 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main {
     public static void main(String[] args) {
         //DbIO io = new DbIO();
         //io.driver();
+        Wardrobe wardrobe = new Wardrobe();
         MainMenu mm = new MainMenu();
         mm.setup();
+
     }
 }
