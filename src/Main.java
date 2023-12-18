950f15c781acc48ffc34d887abce6f24fcd952d1
Patrick Nyboe Kjøller <Patrick@ctrl.dk>
2023-12-18 10:20:43 +0100

    Update2

--- src/Main.java
+++ src/Main.java
@@ -6,25 +6,7 @@ public class Main {
     public static void main(String[] args) {
         DbIO io = new DbIO();
         MainMenu mainMenu = new MainMenu();
-       // User logininuser = mainMenu.login();
         MainMenu.loggedInUser = mainMenu.setup();
-/*
-        if(loggedInUser!=null){
-            System.out.println("Userloggedin:"+loggedInUser.getUsername());
 
-            Wardrobe wardrobe = new Wardrobe(loggedInUser.getUser_id());
-            Connection connection=null;
-            try{
-                connection=io.getConnection();
-            }catch(SQLException e){
-//Handletheexceptionorprintthestacktrace
-                e.printStackTrace();
-            }
-            wardrobe.addClothingToWardrobe(connection);
-        }else{
-            System.out.println("Usernotloggedin.");
-        }
-
-    */
     }
 }
